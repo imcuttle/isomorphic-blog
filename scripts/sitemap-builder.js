427b52441dd3725ee56f7530746630f4e16374e2
Moyu <492899414@qq.com>
2017-02-14 17:56:47 +0800

    sync from server

--- scripts/sitemap-builder.js
+++ scripts/sitemap-builder.js
@@ -46,7 +46,9 @@ const articlePath = path.join(__dirname + '/../source/_articles')
 const rewrite = module.exports =  function () {
     const mapStr = sites.concat(
         fs.readdirSync(articlePath).filter(p => !skipRegExp.test(p)).map(p => p.replace(/\.[^\.]*$/, '')).map(u => '/article/'+u)
-    ).map(u => host+u).join('\r\n')
+    ).map(u => host+u).join('\r\n');
+
+    console.log(mapStr)
 
     return new Promise((resolve, reject) => {
         fs.writeFile(path.join(__dirname, '../source/public/sitemap.txt'), mapStr, function (err) {
