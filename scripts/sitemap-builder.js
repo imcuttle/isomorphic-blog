d2ccb4967239b45e91805c18e72614850a3c7ab0
Moyu <492899414@qq.com>
2017-02-15 09:31:09 +0800

    sitemap.xml

--- scripts/sitemap-builder.js
+++ scripts/sitemap-builder.js
@@ -6,6 +6,7 @@ const path = require('path')
 const sitemapBuilder = require('react-router-sitemap-builder').default
 const skipRegExp = require('../source/config').skipRegExp
 const getSites = require('react-router-sitemap-builder').getSites
+const html_encode = require('../frontend/src/common/utils').html_encode;
 const router = require('../frontend/src/router').routerForSiteMap;
 const fs = require('fs')
 
@@ -42,17 +43,29 @@ const sites = getSites(router) || [];
 
 const articlePath = path.join(__dirname + '/../source/_articles')
 
+const getUrlXMLString = (loc, lastmod, priority) => ["<url>", `<loc>${loc}</loc>`, `<lastmod>${lastmod}</lastmod>`, `<priority>${priority}</priority>`, "</url>"].join('')//reduce((p, n) => n ? (p+n+"\r\n") : p, "")
+
+
+getUrlXMLString({})
+
+
+const sitemapXML = (xmlString) => `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${xmlString}</urlset>`
+
 
 const rewrite = module.exports =  function () {
-    const mapStr = fs.readdirSync(articlePath).filter(p => !skipRegExp.test(p))
-        .map(p => p.replace(/\.[^\.]*$/, '')).map(u => '/article/'+u).concat(
-            sites
-        ).map(u => host+u).join('\r\n');
+    const articleSites = fs.readdirSync(articlePath).filter(p => !skipRegExp.test(p))
+        .map(p => ({mtime: fs.statSync(articlePath+'/'+p).mtime, loc: host+'/article/'+html_encode(p.replace(/\.[^\.]*$/, ''))}) ).sort((a, b) => b.mtime-a.mtime)
 
+
+
+    let mapStr = [getUrlXMLString(host+'/', articleSites[0].mtime.toISOString(), "0.8")].concat(articleSites.map(x => getUrlXMLString(x.loc, x.mtime.toISOString(), "1.0")))
+        .join('')
+
+    mapStr = sitemapXML(mapStr);
     console.log(mapStr)
 
     return new Promise((resolve, reject) => {
-        fs.writeFile(path.join(__dirname, '../source/public/sitemap.txt'), mapStr, function (err) {
+        fs.writeFile(path.join(__dirname, '../source/public/sitemap.xml'), mapStr, function (err) {
             if (err) reject(err);
             else resolve(1);
         });
